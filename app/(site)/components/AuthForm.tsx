2184889237cc343626766141403ec133d9407aa2
Rasoulmadani <ali.rahbary@chmail.ir>
2023-11-05 15:57:10 +0330

    بروزرسانی

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,9 +1,50 @@
-'use client';
+"use client";
 
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { useCallback, useState } from "react";
+type Variant = "LOGIN" | "REGISTER";
 function AuthForm() {
-  return (
-    <div>AuthForm</div>
-  )
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // Axios Register
+    }
+    if (variant === "LOGIN") {
+      // NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth Social Sign In
+  };
+  return <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div className="bg-white"></div>
+  </div>;
 }
 
-export default AuthForm
+export default AuthForm;
