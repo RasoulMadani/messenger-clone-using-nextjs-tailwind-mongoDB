53b40ffcfac81ab02596a3d853b7dc69ce9af942
Rasoulmadani <ali.rahbary@chmail.ir>
2023-11-06 13:21:15 +0330

    درست کردن نوشته روی خط صفحه ورود

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -19,7 +19,7 @@ function AuthForm() {
   const {
     register,
     handleSubmit,
-    //! TODO این چیه  
+    //! TODO این چیه
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
@@ -48,34 +48,56 @@ function AuthForm() {
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-        <form action="" className="space-y-6 " onSubmit={handleSubmit(onSubmit)}>
-        {variant === 'REGISTER' && (
-          <Input id="name" label="Name" register={register} errors={errors} />
-        )}
-        <Input
-          id="email"
-          label="Email address"
-          type="email"
-          register={register}
-          errors={errors}
-        />
-        <Input
-          id="password"
-          label="Password"
-          type="password"
-          register={register}
-          errors={errors}
-        />
-        <div>
-          <Button
-            disabled={isLoading}
-            fullWidth
-            type="submit"
-          >
-            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
-          </Button>
-        </div>
+        <form
+          action=""
+          className="space-y-6 "
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute
+                inset-0
+                flex
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            
+          </div>
+        </div>
       </div>
     </div>
   );
