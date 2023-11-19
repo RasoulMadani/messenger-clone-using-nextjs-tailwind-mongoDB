91f2a82418ce4d3aa8a81239f52a35bfedb49fd2
Rasoulmadani <ali.rahbary@chmail.ir>
2023-11-19 14:50:37 +0330

    محافظت از مسیرهای conversations تا وقتی از سایت بیرون اومد نتواند به آن مسیر ها برگردد

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-    // در اینجا مسیر صفحه ورود رو مشخص می کنیم 
-    // که اگر خارج از سشن بود به این صفحه برود
+  // در اینجا مسیر صفحه ورود رو مشخص می کنیم
+  // که اگر خارج از سشن بود به این صفحه برود
   pages: {
     signIn: "/",
-  }, 
+  },
 });
 export const config = {
-    // در اینجا مسیر های خصوصی که باید محافظت
-    // بشود را می دهیم 
-  matcher: ["/users/:path*"],
+  // در اینجا مسیر های خصوصی که باید محافظت
+  // بشود را می دهیم
+  matcher: ["/users/:path*", "/conversations/:path*"],
 };
